d9f06346fc074496567838d9b1944005ed536280
honnigam <lucasvm87@gmail.com>
2023-01-31 11:26:56 -0300

    update js.2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var numSecreto = 581 
+var numSecreto = parseInt (Math.random()*1001);  //"1001 multiplicando pelo math.rando que é igual à 0,99999"
 
 while (chute != numSecreto) {
-   var chute = prompt('Digite um numero entre 1 e 1000')
+   var chute = prompt('Digite um numero entre 0 e 1000')
 
    if (chute == numSecreto) {
       alert ('você acertou o numero secreto')
