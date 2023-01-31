ce66e682d74892d9c99f88f4fb1c44c0f760c5a6
honnigam <lucasvm87@gmail.com>
2023-01-31 10:16:50 -0300

    update js

---
+++ script.js
@@ -0,0 +1,10 @@
+var numSecreto = 581 
+var chute = prompt('Digite um numero entre 1 e 1000')
+
+if (chute == numSecreto) {
+   alert ('você acertou o numero secreto')
+}
+
+else {
+   alert ('você errou o numero secreto')
+}
